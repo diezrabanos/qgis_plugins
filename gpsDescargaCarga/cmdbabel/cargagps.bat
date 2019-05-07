e3a78416c52c4dbe23241cd9b75606ffd34cd827
Francisco Javier Díez Rábanos <diezrabanos@hotmail.com>
2019-05-07 09:46:39 +0200

    v1.0.0

---
+++ gpsDescargaCarga/cmdbabel/cargagps.bat
@@ -0,0 +1,17 @@
+set parametro=%1
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F usb:
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F usb:
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F COM1
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F COM1
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F COM2
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F COM2
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F COM3
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F COM3
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F COM4
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F COM4
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F COM5
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F COM5
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F COM6
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F COM6
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -w -i gpx -f %parametro% -o garmin -F COM7
+"C:/Program Files/QGIS 2.18/bin/gpsbabel.exe" -t -i gpx -f %parametro% -o garmin -F COM7

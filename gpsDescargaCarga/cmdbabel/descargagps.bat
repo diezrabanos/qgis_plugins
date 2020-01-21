58658130bd962a16b820f79dbad2900e8365fa1e
Francisco Javier Díez Rábanos <diezrabanos@hotmail.com>
2020-01-21 15:20:15 +0100

    v1.0.4
    elimina la necesidad de bat

--- gpsDescargaCarga/cmdbabel/descargagps.bat
+++
@@ -1,9 +0,0 @@
-set parametro=%1
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f usb: -o gpx -F %parametro%
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f COM1 -o gpx -F %parametro%
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f COM2 -o gpx -F %parametro%
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f COM3 -o gpx -F %parametro%
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f COM4 -o gpx -F %parametro%
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f COM5 -o gpx -F %parametro%
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f COM6 -o gpx -F %parametro%
-"C:/Program Files/QGIS 3.10/bin/gpsbabel.exe" -w -r -t -i garmin -f COM7 -o gpx -F %parametro%
